964be050686806fbb43b9cf3a8b99ebd8c736e20
JayrAlencar <jayralencarpereira@gmail.com>
2016-04-15 08:46:47 -0300

    Person

--- app/person.component.ts
+++ app/person.component.ts
@@ -3,5 +3,10 @@ import {Component} from 'angular2/core';
   templateUrl:"views/person.html"
 })
 export class PersonComponent {
-	
+	private persons: PersonComponent[];
+	constructor(){
+		this.persons = [
+			{id: 1, name:'Jayr', phone:"8786732"}
+		]
+	}
 }
