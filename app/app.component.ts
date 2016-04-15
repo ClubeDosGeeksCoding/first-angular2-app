c82d2898d5ca735545ea49c662ed2995143f469b
JayrAlencar <jayralencarpereira@gmail.com>
2016-04-14 22:20:24 -0300

    Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {PersonComponent}   from './person.component.ts';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'views/index.html',
+  	directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+
+  	{path:'/persons',    name: 'Persons',   component: PersonComponent}
+])
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component.ts';
+import {AppComponent}     from './app.component.ts';
+import {bootstrap}        from 'angular2/platform/browser';
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from 'angular2/router';
+import {provide}           from 'angular2/core';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS
+]);
